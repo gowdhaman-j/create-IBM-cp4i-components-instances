eff77079e16d946892780257edb6464af0a10b84
gjayasee <gjayasee@in.ibm.com>
2020-05-07 15:28:14 +0530

    Update override-values-yaml.sh

--- Setup-SingleReselientQM/override-values-yaml.sh
+++ Setup-SingleReselientQM/override-values-yaml.sh
@@ -17,6 +17,13 @@ export MY_qmPVC_storageClassName=ibmc-file-silver
 # Name of the Queue Manager you want to assign
 export MY_queueManager_name=QM
 
+# Tracing specific properties
+#Disable the Tracing
+export MY_odTracingConfig_enabled=false
+# Namespace of the tracing component whete tracing release is running
+export MY_odTracingNamespace=
+
+
 
 ############ Let us find & Replace the $RELEASE_NAME-values.yaml
 
@@ -29,6 +36,6 @@ sed -i -e 's, MY_dataPVC_storageClassName, '$MY_dataPVC_storageClassName',g' $RE
 sed -i -e 's, MY_logPVC_storageClassName, '$MY_logPVC_storageClassName',g' $RELEASE_NAME-values.yaml
 sed -i -e 's, MY_qmPVC_storageClassName, '$MY_qmPVC_storageClassName',g' $RELEASE_NAME-values.yaml
 sed -i -e 's, MY_queueManager_name, '$MY_queueManager_name',g' $RELEASE_NAME-values.yaml
-
-
+sed -i -e 's, MY_odTracingConfig_enabled, '$MY_odTracingConfig_enabled',g' $RELEASE_NAME-values.yaml
+sed -i -e 's, MY_odTracingNamespace, '$MY_odTracingNamespace',g' $RELEASE_NAME-values.yaml
 
