d75bd4a738dcb2fb2005ecc1f16d6c2b3202cb33
gjayasee <gjayasee@in.ibm.com>
2020-05-07 17:17:21 +0530

    Update override-values-yaml.sh

--- Setup-ACE-DashBoard/override-values-yaml.sh
+++ Setup-ACE-DashBoard/override-values-yaml.sh
@@ -1,36 +1,26 @@
 #!/bin/sh
 
 
+export MY_license=accept
 
 export MY_pullSecret=$PULL_SECRET_NAME
 
-export MY_redis-ha_replica_servers=1
+export MY_helmTlsSecret=$HELM_TLS_SECRET_NAME
 
-export MY_redis-ha_replica_sentinels=1
+export MY_productionDeployment=false
 
-export MY_assetSync_storageClassName=ibmc-file-silver
+export MY_storageClassName=ibmc-file-silver
 
-export MY_assetSync_replicas=1
-
-export MY_assetUI_replicas=1
-
-export MY_couchdb_replicas=1
-
-export MY_couchdb_storageClass=ibmc-block-silver
-
+export MY_replicaCount=1
 
 
 ############ Let us find & Replace the $RELEASE_NAME-values.yaml
 
 
 sed -i -e 's, MY_pullSecret, '$MY_pullSecret',g' $RELEASE_NAME-values.yaml
-sed -i -e 's, MY_redis-ha_replica_servers, '$MY_redis-ha_replica_servers',g' $RELEASE_NAME-values.yaml
-sed -i -e 's, MY_redis-ha_replica_sentinels, '$MY_redis-ha_replica_sentinels',g' $RELEASE_NAME-values.yaml
-sed -i -e 's, MY_assetSync_storageClassName, '$MY_assetSync_storageClassName',g' $RELEASE_NAME-values.yaml
-sed -i -e 's, MY_assetSync_replicas, '$MY_assetSync_replicas',g' $RELEASE_NAME-values.yaml
-sed -i -e 's, MY_assetUI_replicas, '$MY_assetUI_replicas',g' $RELEASE_NAME-values.yaml
-sed -i -e 's, MY_couchdb_replicas, '$MY_couchdb_replicas',g' $RELEASE_NAME-values.yaml
-sed -i -e 's, MY_couchdb_storageClass, '$MY_couchdb_storageClass',g' $RELEASE_NAME-values.yaml
-
-
+sed -i -e 's, MY_license, '$MY_license',g' $RELEASE_NAME-values.yaml
+sed -i -e 's, MY_helmTlsSecret, '$MY_helmTlsSecret',g' $RELEASE_NAME-values.yaml
+sed -i -e 's, MY_productionDeployment, '$MY_productionDeployment',g' $RELEASE_NAME-values.yaml
+sed -i -e 's, MY_storageClassName, '$MY_storageClassName',g' $RELEASE_NAME-values.yaml
+sed -i -e 's, MY_replicaCount, '$MY_replicaCount',g' $RELEASE_NAME-values.yaml
 
